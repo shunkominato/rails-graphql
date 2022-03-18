2088b6d1bbe62a3c34d311378960431b88ad9b22
shunkominato <shun19871001@gmail.com>
2022-03-19 08:16:32 +0900

    graphql

--- db/migrate/20220318085928_create_posts.rb
+++ db/migrate/20220318085928_create_posts.rb
@@ -1,10 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :body
+      t.string :title, null: false
+      t.text :body, null: false
 
       t.timestamps
     end
+    add_index :posts, :created_at
   end
 end
--- db/migrate/20220318085937_create_comments.rb
+++ db/migrate/20220318085937_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.references :post, null: false, foreign_key: true
-      t.text :body
+      t.text :body, null: false
 
       t.timestamps
     end
