0785ef53b3ed601acf1fa7409e7fc71f3a763444
shunkominato <shun19871001@gmail.com>
2022-03-19 10:19:23 +0900

    mutaiton

--- app/graphql/mutations/create_post.rb
+++ app/graphql/mutations/create_post.rb
@@ -1,14 +1,20 @@
 module Mutations
-  class CreatePost < BaseMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+  class CreatePost < GraphQL::Schema::Mutation
+    # graphql_name 'CreatePost'
+    type Types::PostType, null: false
 
-    # TODO: define arguments
-    # argument :name, String, required: true
-
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    argument :body, String, required: true
+    argument :title, String, required: true
+ 
+    def resolve(title: nil, body: nil)
+      p "--------------"
+      Rails.logger.debug('111')
+      p "--------------"
+      post = Post.create!(
+        title: title,
+        body: body
+      )
+      { post: post }
+    end
   end
 end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,11 +1,5 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_post, mutation: Mutations::CreatePost
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
   end
 end
